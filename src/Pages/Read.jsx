916f659739b9ac450baaab39b30e9ac31ce4b5fa
AswinBlue <aswindblew@gmail.com>
2022-09-26 23:02:45 +0900

    (ADD) Read.jsx
    - Read 페이지 작성
    - GridComponent 적용하여 표 생성
    - App.css에 syncfusion 내용 추가

--- src/Pages/Read.jsx
+++ src/Pages/Read.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../Data/dummy';
+import { Header } from '../Components'
 
 const Read = () => {
   return (
-    <div>Read</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Read'/>
+      {/* 표 세팅. 데이터는 json 형태로 받아옴*/}
+      <GridComponent id='gridcomp' dataSource={ordersData}>
+        {/* 컬럼 제목 표시 */}
+        <ColumnsDirective>
+          {/* 반복문으로 Json에서 데이터 받아와서 사용. dataSource와 item의 field에 맞게 알아서 세팅됨 */}
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
   )
 }
 
